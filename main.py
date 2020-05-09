61992a2dfb854331fb9bcd7ed6e529d86eb53c5d
Cimar Rodrigo Morales <rdo15072001@gmail.com>
2020-05-08 20:08:02 -0400

    Primer commit

---
+++ main.py
@@ -0,0 +1,54 @@
+import pygame
+import Colors
+from Invader import Invader
+
+pygame.init()
+window_width = 800
+window_height = 600
+window_size = (window_width, window_height)
+main_window = pygame.display.set_mode(window_size)
+
+pygame.display.set_caption("Project")
+
+y = 200  # player
+x = 200  # player
+
+width = 20
+height = 20
+
+vel = 10
+
+running_game = True
+
+inv_1 = Invader(20, 30)
+
+while running_game:
+    pygame.time.delay(10)
+    main_window.fill(Colors.black)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running_game = False
+            break
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT] and x > 0:
+        x -= vel
+
+    if keys[pygame.K_RIGHT] and x < 800 - width:
+        x += vel
+
+    if keys[pygame.K_UP] and y > 0:
+        y -= vel
+
+    if keys[pygame.K_DOWN] and y < 600 - height:
+        y += vel
+
+    inv_1.draw(main_window)
+    inv_1.move_in_y(main_window)
+
+    pygame.draw.rect(main_window, Colors.red, (x, y, width, height))
+
+    pygame.display.update()
+
+pygame.quit()
